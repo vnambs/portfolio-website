e9247318f07148c98889b6eefc533e05bf8290d5
Voary Nambinina <vnambs@gmail.com>
2024-04-08 15:32:58 +0300

    build: upgrade storybook and change header logo

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -25,7 +25,6 @@ const customViewports = Object.fromEntries(
 );
 
 export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
--- src/components/Header/Header.stories.ts
+++ src/components/Header/Header.stories.ts
@@ -10,6 +10,6 @@ export default {
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const BasicHeader = {
   args: {
-    logo: 'Test'
+    logo: 'Jonathan Voary Nambinina',
   },
 };
